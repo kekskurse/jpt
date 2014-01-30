9dc6df78b4ab42f690943b1465ddd5d8da90c7c4
sspssp <soeren@siegismund-poschmann.de>
2014-01-30 19:00:54 +0100

    gendern

--- templates/loko/invite.php
+++ templates/loko/invite.php
@@ -27,12 +27,13 @@ Ohai,
 
 am 03.<?php if(date("m")+1<10){echo"0";}?><?php echo date("m")+1; ?>.<?php echo date("Y"); ?> um 21:00 findet das nächste Lokalkoordinations-Mumble statt. Auf diesem möchten wir den Austausch der unterschiedlichen lokalen Gruppen fördern. Auf dem Treffen sollen alle Gruppen ihre aktuellen Planungen für Aktionen u.ä. ausgiebig vorstellen. Hierdurch könnt ihr durch Erfahrungen anderer Gruppen und umgekehrt andere von euren Erfahrungen profitieren.
 
-Außerdem könnt ihr die Veranstaltung nutzen um Fragen zur Organisation oder an den Bundesvorstand zu stellen. Wir bitten darum, dass aus jeder Gruppe mindestens ein Vertreter zu dem Treffen erscheint. Die Tagesordnung findet ihr in folgendem Pad:
+Außerdem könnt ihr die Veranstaltung nutzen um Fragen zur Organisation oder an den Bundesvorstand zu stellen. Wir bitten darum, dass aus jeder Gruppe mindestens ein*e Vertreter*in zu dem Treffen erscheint. Die Tagesordnung findet ihr in folgendem Pad:
 
 http://pad.junge-piraten.de/loko-<?php echo date("Y"); ?>-<?php if(date("m")+1<10){echo"0";}?><?php echo date("m")+1; ?>-03
 
+
 Viele Grüße,
-LoKoDerps
+LoKoderps
 </textarea><br>
 <input type="checkbox" class="form-controll" name="test" checked="checked">Test Einladung (Wird nur im Test Forum gepostet)<br>
         <input type="submit" class="btn btn-default">
